419669f390f38586c2b6b9abd3e82df5d09b2f4a
EYToptas <emirhanyusuftr@gmail.com>
2025-05-11 03:09:08 +0300

    Update CustomerMainPage.java
    şifre değiştirme butonu eklendi

--- src/yazilim/CustomerMainPage.java
+++ src/yazilim/CustomerMainPage.java
@@ -55,65 +55,6 @@ public class CustomerMainPage {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	/*
-	private void initialize() {
-		frame = new JFrame();
-		frame.setTitle("Müşteri Ana Sayfası");
-		frame.setBounds(100, 100, 450, 230);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JButton offerButton = new JButton("Fiyat Teklifi Al");
-		offerButton.setFocusable(false);
-		offerButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.setVisible(false);
-				PriceOfferRequestPage offerPage = new PriceOfferRequestPage(usrId, conn);
-				offerPage.showFrame();
-			}
-		});
-		offerButton.setFont(new Font("Tahoma", Font.BOLD, 16));
-		offerButton.setBounds(150, 30, 200, 30);
-		frame.getContentPane().add(offerButton);
-		
-		
-		JButton testDriveButton = new JButton("Test Sürüşü Talep Et");
-		testDriveButton.setFocusable(false);
-		testDriveButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				frame.setVisible(false);
-				TestDriveRequestPage testDrivePage = new TestDriveRequestPage(usrId, conn);
-				testDrivePage.showFrame();
-			}
-		});
-		testDriveButton.setFont(new Font("Tahoma", Font.BOLD, 16));
-		testDriveButton.setBounds(150, 80, 200, 30);
-		frame.getContentPane().add(testDriveButton);
-		
-		JButton returnButton = new JButton("Çıkış Yap");
-		returnButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				StartPage start_page;
-				start_page = new StartPage(conn);
-				start_page.showFrame();
-				frame.setVisible(false);
-			}
-		});
-		returnButton.setFont(new Font("Tahoma", Font.BOLD, 18));
-		returnButton.setBounds(150, 130, 200, 30);
-		frame.getContentPane().add(returnButton);
-		returnButton.setFocusable(false);
-		JButton changePasswordButton = new JButton("Şifre Değiştir");
-		changePasswordButton.setBounds(100, 160, 200, 30);
-		frame.getContentPane().add(changePasswordButton);
-
-		changePasswordButton.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {
-		        new CustomerChangePassword(conn, usrId);
-		    }
-		});
-	}
-	*/
 	
 	private void initialize() {
 	    frame = new JFrame();
